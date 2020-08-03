8c7bc4271358d7d9d7b0a590387454ce580ffe33
KengoA <kengo@hey.com>
2020-08-03 04:04:50 +0100

    modified run bash & ignore graphs and pycache

--- cooccur/run.sh
+++ cooccur/run.sh
@@ -1,16 +1,16 @@
 # # Run download.sh first if no data directory is empty
 # # Create intersection vocab
-# python ./vocab/make_vocab.py
+python ./vocab/make_vocab.py
 
 # # Count cooccurrence for images
-# cd openimages
-# python ./src/aggregate_data.py
-# python ./src/count_cooccur.py
+cd openimages
+python ./src/aggregate_data.py
+python ./src/count_cooccur.py
 
-# cd ..
+cd ..
 cd enwiki
-# split --verbose -l 5000 data/raw/enwiki_201308.txt data/streams/enwiki_201308_
-# python src/preprocess.py
+split --verbose -l 5000 data/raw/enwiki_201308.txt data/streams/enwiki_201308_
+python src/preprocess.py
 cd src/cython
 python setup.py build_ext --inplace
 python run.py
