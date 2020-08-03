11473417042747c1231810071fd65a9babe88507
KengoA <kengo@hey.com>
2020-08-03 03:57:59 +0100

    added download and extract commands for enwiki 2013 Aug dump;

--- cooccur/download.sh
+++ cooccur/download.sh
@@ -1,3 +1,4 @@
+### Image: Open Images ###
 # download class names
 wget -O ./vocab/class_names.csv https://storage.googleapis.com/openimages/v5/class-descriptions-boxable.csv
 
@@ -6,6 +7,20 @@ wget -O ./open-images/data/raw/train.csv https://storage.googleapis.com/openimag
 wget -O ./open-images/data/raw/validation.csv https://storage.googleapis.com/openimages/v5/validation-annotations-human-imagelabels-boxable.csv
 wget -O ./open-images/data/raw/test.csv https://storage.googleapis.com/openimages/v5/test-annotations-human-imagelabels-boxable.csv
 
-# # download and extract wikipedia dump from 2013 August
-# wget 
-# bzip2 -d 
+
+### Text: Wikipedia Dump ###
+# download and extract wikipedia dump from 2013 August
+export fileid=1uBbwHMCPvX9QeOoEfl68dy7jWj4iF87E
+export filename=enwiki/data/raw/enwiki_201308.txt.bz2
+
+## WGET ##
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+
+wget --load-cookies cookies.txt -O $filename \
+     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
+
+rm confirm.txt cookies.txt
+
+cd enwiki/data/raw/
+bzip2 -d -v -k enwiki_201308.txt.bz2
